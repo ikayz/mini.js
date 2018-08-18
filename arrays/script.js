f5dcba5520906976f8aa9f01d34c12b115c8e712
ikayz <ikayz@live.com>
2018-08-18 23:20:34 +0200

    Changing and accessing items in arrays

--- arrays/script.js
+++ arrays/script.js
@@ -1,6 +1,14 @@
+// Declaring and assigning values to arrays
 var books, movies;
 books = ["The Hobbit", "The last of the Mohicans", "Harry Potter"];
 movies = ["Lord of the Rings", "The Great Gatsby", "Titanic"];
 
 console.log(books[1]);
 console.log(movies[2]);
+
+// Changing an item in an array
+books[2] = "John Wick";
+movies[1] = "King Arthur";
+
+console.log(books);
+console.log(movies);
