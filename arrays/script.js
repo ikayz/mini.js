4609a6b1bca60dd45883a32b4550c75a46bcdf8c
ikayz <ikayz@live.com>
2018-08-18 23:14:44 +0200

    Arrays and usage

---
+++ arrays/script.js
@@ -0,0 +1,6 @@
+var books, movies;
+books = ["The Hobbit", "The last of the Mohicans", "Harry Potter"];
+movies = ["Lord of the Rings", "The Great Gatsby", "Titanic"];
+
+console.log(books[1]);
+console.log(movies[2]);
